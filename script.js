fa799d284691bb6993caf4057d16d85d8281a834
Albin Lindell <albinglindell98@gmail.com>
2021-10-25 08:20:25 +0200

    fixade sista funktionerna och css

--- script.js
+++ script.js
@@ -6,16 +6,32 @@ let allQuestions = document.querySelectorAll("input")
 let labels = document.querySelectorAll("label")
 let legends = document.querySelectorAll("legend")
 let game= document.querySelector(".game")
+let startingScreen= document.querySelector(".startingScreen")
 let score= document.querySelector(".allTheRights")
 let header = document.querySelectorAll("h2")
 let felCheck = document.querySelectorAll(".check7")
 let text= document.querySelectorAll("h1")
+let testing = document.querySelector(".score")
 
 
+
+let correctQuiz =()=>{
+    if(rightAnsweres>=10*0.5 && rightAnsweres< 10*7.5){
+        score.style.color="orange"
+        score.innerHTML=`<h3> Du fick ${rightAnsweres} många poäng</h3>`
+    }if(rightAnsweres > 10*0.75){
+        score.style.color="green"
+        score.innerHTML=`<h3> Du fick ${rightAnsweres} många poäng</h3>`
+    }if (rightAnsweres < 10*0.5) {
+        score.style.color="red"
+        score.innerHTML=`<h3> Du fick ${rightAnsweres} många poäng</h3>`
+
+    }
+}
 
 
 //startar spelet!
-
+let playing = 0
 let darkLight = 0
 let rightAnsweres = 0
 let checkbox1 = 0
@@ -35,16 +51,11 @@ switchModeBtn.addEventListener("click", ()=>{
     header.forEach(e=>{
         e.style.color="white"
     })
-    if(rightAnsweres>=10*0.5 && rightAnsweres< 10*7.5){
-        score.style.color="orange"
-    }if(rightAnsweres > 10*0.75){
-        score.style.color="green"
-    }if(rightAnsweres < 10*0.5){
-        score.style.color="white"   
-    }
-
-
+    if(score>0){
+        
+    correctQuiz()
     
+    }
     darkLight=1
     }else if(darkLight===1){
         document.body.style.backgroundColor="white"
@@ -60,20 +71,18 @@ switchModeBtn.addEventListener("click", ()=>{
     header.forEach(e=>{
         e.style.color="black"
     })
-    
-    if(rightAnsweres>=10*0.5 && rightAnsweres< 10*7.5){
-        score.style.color="orange"
-    }if(rightAnsweres > 10*0.75){
-        score.style.color="green"
-    }if(rightAnsweres < 10*0.5){
-        score.style.color="black"   
+    if(score>0){
+        
+        correctQuiz()
     }
+    
     darkLight=0
     }
 })
 
 startGame.addEventListener("click", ()=>{
     game.style="display: block;"
+startingScreen.style="display:none"
     console.log(game)
 })
 
@@ -84,12 +93,16 @@ restartBtn.addEventListener("click",()=>{
     })
     score.innerHTML=""
     game.style="display: none;"
+    startingScreen.style="display: flex;"
     rightAnsweres = 0
     checkbox1 = 0
+    playing = 0
 
 })
 
 checkBtn.addEventListener("click", ()=>{
+         if(playing===0){
+            playing=1
          allQuestions.forEach(e=>{
         if(e.checked){
             if(e.id==="right"){  
@@ -114,25 +127,11 @@ console.log(checkbox1)
         
     })
     
+    correctQuiz()
     
-        
-
-    if(rightAnsweres>=10*0.5 && rightAnsweres< 10*7.5){
-        score.style.color="orange"
-        score.innerHTML=`<h1> Du fick ${rightAnsweres} många poäng</h1>`
-    }if(rightAnsweres > 10*0.75){
-        score.style.color="green"
-        score.innerHTML=`<h1> Du fick ${rightAnsweres} många poäng</h1>`
-    }if (darkLight===1 && rightAnsweres < 10*0.5) {
-        score.style.color="white"
-        score.innerHTML=`<h1> Du fick ${rightAnsweres} många poäng</h1>`
-
-    }if(darkLight===0 && rightAnsweres < 10*0.5){
-        score.style.color="black"
-        score.innerHTML=`<h1> Du fick ${rightAnsweres} många poäng</h1>`
-    }
-
-    rightAnsweres=0
-    checkbox1=0
     
+   
+}
 })
+
+
